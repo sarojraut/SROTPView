47940e00716076a1ff57c367742b2e306a73c5a1
saroj raut <saroj.raut@swifttech.com.np>
2019-11-21 12:25:21 +0545

    Initial Commit

---
+++ SROTPView/SROTPView.h
@@ -0,0 +1,19 @@
+//
+//  SROTPView.h
+//  SROTPView
+//
+//  Created by admin on 11/21/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SROTPView.
+FOUNDATION_EXPORT double SROTPViewVersionNumber;
+
+//! Project version string for SROTPView.
+FOUNDATION_EXPORT const unsigned char SROTPViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SROTPView/PublicHeader.h>
+
+
